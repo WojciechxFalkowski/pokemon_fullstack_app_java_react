672a6a6642fb9bc72f19dc7cfef5f718b3044f92
Wojtek Falkowski <wojciechxfalkowski@gmail.com>
2024-10-27 16:21:13 +0100

    wip

--- Pokemon API/src/main/java/com/pokemon/api/config/WebConfig.java
+++ Pokemon API/src/main/java/com/pokemon/api/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:5173", "https://pokemon-app.wojciechfalkowski.pl", "http://raspberrypi.local:7010")
+                        .allowedOrigins("http://localhost:5173", "https://pokemon-app.wojciechfalkowski.pl", "http://raspberrypi.local:7010", "https://pokemon-api.wojciechfalkowski.pl")
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
                         .allowedHeaders("*");
             }
